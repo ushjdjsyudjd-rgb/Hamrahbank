49dff4cc214e06df7d8c5b88ca8a2398199437b8
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2025-12-27 11:44:40 +0330

    Change cache version and asset paths in sw.js
    Updated cache version and modified asset paths.

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
-const CACHE_NAME = 'bank-cache-v3';
+const CACHE_NAME = 'bank-v5'; // تغییر ورژن برای آپدیت کش
 const assets = [
-  './',
-  './index.html',
-  './manifest.json',
+  '/Hamrahbank/',
+  '/Hamrahbank/index.html',
+  '/Hamrahbank/manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
-// نصب و ذخیره فایل‌ها
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
 });
 
-// فعال‌سازی و پاکسازی کش قدیمی
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// پاسخگویی به درخواست‌ها
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
